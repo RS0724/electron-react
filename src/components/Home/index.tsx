467e292372441287b85c6ac2a497342adddcbc19
RS0724 <rosesilva0724@gmail.com>
2023-10-23 19:13:04 -0300

    feat: add partial home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Flex, Input, TagLabel } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 interface Iprops {
@@ -5,17 +6,36 @@ interface Iprops {
 }
 
 const Home = ({ setLogged }: Iprops) => {
-    const navigate = useNavigate();
-  
-    const Logout = () => {
+  const navigate = useNavigate();
+
+  const Logout = () => {
     localStorage.clear();
     setLogged(false);
     navigate("/");
   };
+
   return (
     <>
       <h1>Home</h1>
       <button onClick={Logout}>Logout</button>
+      <Flex 
+      border={"1px solid black"} 
+      width={"60rem"} 
+      height={"40rem"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      >
+        <label>Token bot:</label>
+        <Input
+          id="token-bot"
+          width={"80%"}
+          height={"1rem"}
+          padding={"0.5rem"}
+        />
+        
+        <label>Token bot:</label>
+        <Input id="chat-id" width={"80%"} height={"1rem"} padding={"0.5rem"} />
+      </Flex>
     </>
   );
 };
