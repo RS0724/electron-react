60458103d3e3e8057087af16d0439f85f9406cc0
RS0724 <rosesilva0724@gmail.com>
2023-10-31 19:34:50 -0300

    feat: add component RecoverinformTokenEmail

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -11,6 +11,7 @@ import { useEffect } from "react";
 import NewUser from "../components/NewUser";
 import RecoverPassword from "../components/RecoverPassword";
 import RecoverInformEmail from "../components/RecoverInformEmail";
+import RecoverInformTokenEmail from "../components/RecoverInformTokenEmail";
 
 interface Iprops {
   logged: boolean;
@@ -28,7 +29,7 @@ const Routes = ({ logged, setLogged }: Iprops): JSX.Element => {
             logged ? <Navigate to={"/home"} /> : <Navigate to={"/login"} />
           }
         />
-        <Route path="/login" element={<RecoverInformEmail />} />
+        <Route path="/login" element={<Login setLogged={setLogged} />} />
         <Route path="/home" element={<Home setLogged={setLogged} />} />
         <Route path="/register" element={<NewUser />} />
         <Route path="/recover" element={<RecoverPassword />} />
