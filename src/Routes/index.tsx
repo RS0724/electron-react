458b00ee9c69f91f2aa51ae9ffc3ef99cec62f20
RS0724 <rosesilva0724@gmail.com>
2023-10-31 19:17:02 -0300

    feat: add componentRecoverinformEmail

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,9 +1,16 @@
-import { HashRouter, Navigate, Route, Routes as Switch, useFetcher } from "react-router-dom";
+import {
+  HashRouter,
+  Navigate,
+  Route,
+  Routes as Switch,
+  useFetcher,
+} from "react-router-dom";
 import Login from "../components/Login";
 import Home from "../components/Home";
 import { useEffect } from "react";
 import NewUser from "../components/NewUser";
 import RecoverPassword from "../components/RecoverPassword";
+import RecoverInformEmail from "../components/RecoverInformEmail";
 
 interface Iprops {
   logged: boolean;
@@ -11,18 +18,23 @@ interface Iprops {
 }
 
 const Routes = ({ logged, setLogged }: Iprops): JSX.Element => {
-  useEffect(()=>{},[logged])
+  useEffect(() => {}, [logged]);
   return (
     <HashRouter>
       <Switch>
-        <Route path="/" element={logged ? <Navigate to={"/home"}/>: <Navigate to={"/login"}/>} />
-        <Route path="/login" element={< Login setLogged={setLogged} />} />
-        <Route path="/home" element={< Home setLogged={setLogged} />} />
-        <Route path="/register" element={< NewUser/>} />
-        <Route path="/recover" element={< RecoverPassword/>} />
+        <Route
+          path="/"
+          element={
+            logged ? <Navigate to={"/home"} /> : <Navigate to={"/login"} />
+          }
+        />
+        <Route path="/login" element={<RecoverInformEmail />} />
+        <Route path="/home" element={<Home setLogged={setLogged} />} />
+        <Route path="/register" element={<NewUser />} />
+        <Route path="/recover" element={<RecoverPassword />} />
       </Switch>
     </HashRouter>
   );
 };
- 
+
 export default Routes;
---
+++ src/components/RecoverInformEmail/index.tsx
@@ -0,0 +1,116 @@
+import {
+    Box,
+    Button,
+    Flex,
+    FormControl,
+    FormLabel,
+    Input,
+    Link,
+    Text,
+  } from "@chakra-ui/react";
+  import { useState, useEffect } from "react";
+  import Api from "../../Api";
+  import { useNavigate } from "react-router-dom";
+  
+    const RecoverInformEmail = (): JSX.Element => {
+    const [email, setEmail] = useState<string>("");
+    const navigate = useNavigate()
+  
+    return (
+      <Box
+        width={"100vw"}
+        height={"100vh"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        backgroundImage={
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1PawjGfwwWVARqgH4m-gUrTt0ACLHOYLuFg&usqp=CAU"
+        }
+      >
+        <Box
+          width={"100vw"}
+          height={"100vh"}
+          position={"absolute"}
+          backgroundColor={"#80808080"}
+        ></Box>
+        <Flex
+          margin={"auto"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={"2rem"}
+          borderRadius={"1rem"}
+          width={"20%"}
+          justifyContent={"center"}
+          height={"35%"}
+          padding={"2rem"}
+          boxShadow={"1rem 1rem 1rem 0"}
+          background={"white"}
+          zIndex={"2"}
+        >
+          <Text fontSize="2rem" fontWeight="bold" mb={4}>
+            Informe Seu Email
+          </Text>
+          <FormControl>
+  
+            <FormLabel>Email:</FormLabel>
+            <Input
+              type="email"
+              placeholder="Digite Sua Email Cadastrado"
+              padding={"0.6rem"}
+              width={"20rem"}
+              borderRadius={"0.5rem"}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </FormControl>
+  
+          <Flex gap={"2rem"}>
+
+          <Button
+        padding={"1rem 3rem"}
+        borderRadius={"1.3em"}
+        fontSize={"1.3rem"}
+        fontWeight={"600"}
+        backgroundColor={"#00a2ff"}
+        color={"white"}
+        border={"none"}
+        cursor={"pointer"}
+        _hover={{
+          color: "#00a2ff",
+          border: "1px solid",
+          background: "white",
+        }}
+        marginBottom={"2rem"}
+       >
+        Enviar
+      </Button>
+
+      <Button
+        padding={"1rem 2rem"}
+        borderRadius={"1.3em"}
+        fontSize={"1.3rem"}
+        fontWeight={"600"}
+        backgroundColor={"#FF0000"}
+        color={"white"}
+        border={"none"}
+        cursor={"pointer"}
+        _hover={{
+          color: "#FF0000",
+          border: "1px solid",
+          background: "white",
+        }}
+        marginBottom={"2rem"}
+        onClick={()=>navigate("/")}
+       >
+        Cancelar
+      </Button>
+  
+          </Flex>
+  
+          </Flex>
+      </Box>
+    );
+  };
+  
+  export default RecoverInformEmail;
